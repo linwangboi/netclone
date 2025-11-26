e49625f19f8e8d1ce34839386e43152ed77ec944
linwangboi <linwangboi@gmail.com>
2025-11-26 11:31:57 +0000

    login view

--- core/views.py
+++ core/views.py
@@ -7,6 +7,16 @@ def index(request):
     return render(request, 'index.html')
 
 def login(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = auth.authenticate(username=username, password=password)
+        if user is not None:
+            auth.login(request, user)
+            return redirect('/')
+        else:
+            messages.info(request, 'Invalid credentials')
+            return redirect('login')
     return render(request, 'login.html')
 
 def signup(request):
