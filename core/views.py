178b304c43aa9cadcdfb07155ee6805d81a0a59d
linwangboi <linwangboi@gmail.com>
2025-11-26 11:29:42 +0000

    signup

--- core/views.py
+++ core/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+import json
+from django.shortcuts import redirect, render
+from django.contrib.auth.models import User, auth
+from django.contrib import messages
 
-# Create your views here.
+def index(request):
+    return render(request, 'index.html')
+
+def login(request):
+    return render(request, 'login.html')
+
+def signup(request):
+    if request.method == 'POST':
+        email = request.POST.get('email')   
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        password2 = request.POST.get('password2')
+        if password == password2:
+            if User.objects.filter(email=email).exists():
+                messages.info(request, 'Email already used')
+                return redirect('signup')
+            elif User.objects.filter(username=username).exists():
+                messages.info(request, 'Username already used')
+                return redirect('signup')
+            else:
+                user = User.objects.create_user(username=username, password=password, email=email)
+                user.save()
+                auth.login(request, user)
+                return redirect('/')
+        else:
+            messages.info(request, 'Passwords do not match')
+            return redirect('signup')
+    return render(request, 'signup.html')
--- netclone/settings.py
+++ netclone/settings.py
@@ -75,13 +75,23 @@ WSGI_APPLICATION = "netclone.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
+
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "database",
+        "USER": "postgres",
+        "PASSWORD": "password",
+        "HOST": "127.0.0.1",
+        "PORT": "5432",
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
@@ -129,3 +139,9 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+
+### 
+CSRF_TRUSTED_ORIGINS = [
+    'https://localhost:8000',
+]
