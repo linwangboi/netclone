28cf7c58ae87047a4a6ab55f29d0675913c25b1a
linwangboi <linwangboi@gmail.com>
2025-11-28 10:18:48 +0000

    .

--- netclone/settings.py
+++ netclone/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = "django-insecure-1soaqh2hoq39l)y_)kf33ja$danxyc11!p2l(r0d7s!l$+8z1r
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*', ]
 
 
 # Application definition
